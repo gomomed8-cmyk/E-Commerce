3d093b522680aa929f1639ba3f3fde121b73e182
gomomed8-cmyk <gomomed8@gmail.com>
2026-06-29 21:02:29 +0300

    Add project files.

---
+++ E_Commerce.Infrastructure/Data/StoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce.Domain.Entites.Products;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Data
+{
+    internal class StoreDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+        }
+
+    }
+
+}
